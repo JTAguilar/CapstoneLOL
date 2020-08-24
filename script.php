a7d33cacf848a4670909b7806c492f1ff7b35bfe
Juan Torres Aguilar <jtaguilar@student.neumont.edu>
2020-08-24 09:54:12 -0600

    test consolelog

--- script.php
+++ script.php
@@ -13,9 +13,17 @@ if(isset($_GET['userName'])){
     $loadPlayerData = $proxyurl . 'https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' . $userName . $ApiKey
     $playerdata_JSON = file_get_contents($loadPlayerData);
     $playerdata_array = json_decode($playerdata_JSON, true);
+    console_log($playerdata_array);
 }
 
 
 
-
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
 ?>
