afa198da8343d8a4bb551a17d252008e1a3af02b
Juan Torres Aguilar <jtaguilar@student.neumont.edu>
2020-08-30 13:48:06 -0600

    getting help

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-
-
 var Playerdata;
 var MatchID;
 var request = new XMLHttpRequest();
-const ApiKey = 'RGAPI-1642ace6-8ca9-4c76-b7a9-682ecd4984e4';
+const ApiKey = 'RGAPI-f43298a0-e3c5-4326-a19a-41cedb525bfc';
 const proxyurl = 'https://cors-anywhere.herokuapp.com/';
 var params = new URLSearchParams(window.location.search);
 var Summoner = params.get('userName');
 var games = [];
+const filereader = new FileReader();
 
     function loadPlayerData(username) {
     
@@ -27,10 +26,10 @@ var games = [];
         request.onload = MatchStatsLoadComplete;
         request.send();
     }
-    function GetchampIMG(file, callback){
-        request.open('GET', file, true);
-        request.send(null);
-    }
+    //function GetchampIMG(file, callback){
+    //    request.open('GET', file, true);
+    //   request.send(null);
+    //}
         
 
     function callAPI(){
@@ -72,17 +71,16 @@ var games = [];
     function MatchStatsLoadComplete(evt) {
         var username = params.get('userName').toLowerCase();
         Gamestats = JSON.parse(request.responseText);
-        console.log(Gamestats);
-
-       // let elem = document.createElement('div');
+        //console.log(Gamestats);
+        // let elem = document.createElement('div');
         for (var s = 0; s <9; s++){
           var participant = Gamestats.participantIdentities[s].player.summonerName.toLowerCase();
           if (participant == username){
                 var participantID = s;
-                    for(var game = 0; game <1; game++){
+                for(var game = 0; game <1; game++){
                     
-                        //elem.append(tmpl.content.cloneNode(true));
-                       // document.body.append(elem);
+                //elem.append(tmpl.content.cloneNode(true));
+                // document.body.append(elem);
 
                         if(games[game] == Gamestats.gameId){
                             var winCondition = Gamestats.participants[participantID].stats.win
@@ -95,20 +93,16 @@ var games = [];
                             document.getElementById("Level").innerHTML = Gamestats.participants[participantID].stats.champLevel
                             document.getElementById("CS").innerHTML = Gamestats.participants[participantID].stats.totalMinionsKilled
                             for(players = 0; players < 10; players++){
-                                /*GetchampIMG('/10.16.1/data/en_US/champion/championFull.json', function(text){
-                                    var champim = JSON.parse(text);
+                                //GetchampIMG('/10.16.1/data/en_US/champion/championFull.json', function(text){
+                                    var json = filereader.readAsText('./championFull.json');
+                                    console.log(json)
+                                    var champim = JSON.parse(json);
                                     console.log(champim)
-                                    forEach(chimpim.keys){
-                                    champids = champim.keys;
-                                    if(Gamestats.participants[players].championId === champim.keys[champids]){
-                                    var champname = champim.keys[champids];
-                                        image = (champname + '.jpg');
-                                    }
-                                    }
-                                document.getElementById("Isumm" + players).innerHTML = ('/10.16.1/img/champion' + image);
-                                }*/
-                            document.getElementById("summ" + players).innerHTML = Gamestats.participantIdentities[players].player.summonerName
-                            }
+                                    var champindex = fruits.indexOf(champim);
+                                    var champName = champindex.toString();
+                                    console.log(champname);
+                                    //document.getElementById( "summ" + players ).innerHTML = Gamestats.participantIdentities[players].player.summonerName
+                            };
                             if(winCondition == true){
                                 document.getElementById("result").innerHTML = 'Victory';
                                 div.classList.add('MatchV');
@@ -120,9 +114,8 @@ var games = [];
                                  break;
 				            }
 					    }
-				    }
-           }
-	     }
-    } 
-
-    
+				}
+                }
+          }
+	    
+    };
